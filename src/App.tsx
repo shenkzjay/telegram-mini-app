b2d3703bf331c456b7844752c3723dca9e3a5d3e
shenkzjay <senksjay@gmail.com>
2024-11-06 12:10:32 +0100

    added photo url

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ interface UserData {
   first_name: string;
   last_name?: string;
   username?: string;
+  photo_url?: string;
 }
 
 function App() {
@@ -19,13 +20,16 @@ function App() {
     if (WebApp.initDataUnsafe.user) {
       setUserData(WebApp.initDataUnsafe.user as UserData);
     }
-  }, []);
+  });
 
   return (
     <main>
       <div>
         {userData ? (
           <ul>
+            <li>
+              <img src={userData.photo_url} width={100} height={100} alt={userData.photo_url} />
+            </li>
             <li>ID: {userData.id}</li>
             <li>first_name: {userData.first_name}</li>
             <li>last_name: {userData.last_name}</li>
