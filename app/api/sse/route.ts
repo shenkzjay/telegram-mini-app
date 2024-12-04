f0a743318702ac03ddb4043e1d2db00a4ffed75d
shenkzjay <senksjay@gmail.com>
2024-12-04 19:56:22 +0100

    fixed deployment issues

--- app/api/sse/route.ts
+++ app/api/sse/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import emitter from "@/app/lib/emitter";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
   const response = new NextResponse(
     new ReadableStream({
